6c497567cd9b29b7ced70249c175a5cd79953717
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-02-28 08:20:26 +0530

    Time: 127 ms (43.02%), Space: 25.5 MB (32.07%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -76,29 +76,31 @@
 class Solution {
 public:
     std::string longestPalindrome(std::string s) {
-        if (s.length() <= 1) {
-            return s;
-        }
+        int n =s.size();
+        if(n<=1) return s;
         
-        int max_len = 1;
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
         int start = 0;
         int end = 0;
-        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        int max_len = 1;
         
-        for (int i = 0; i < s.length(); ++i) {
+        for(int i =0;i<n;i++)
+        {
             dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+            for(int j =0;j<i;j++)
+            {
+                if(s[j] == s[i] && (i-j <=2 || dp[j+1][i-1]))
+                {
                     dp[j][i] = true;
-                    if (i - j + 1 > max_len) {
-                        max_len = i - j + 1;
+                    if(i-j+1 > max_len)
+                    {
+                        max_len = i-j+1;
                         start = j;
                         end = i;
                     }
                 }
             }
         }
-        
-        return s.substr(start, end - start + 1);
+        return s.substr(start , end - start +1 );
     }
 };
