054b03c01fc77ad656e0308c5ff924747936ce1c
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-03-17 16:59:06 +0530

    Time: 39 ms (96.66%), Space: 31.1 MB (92.58%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX - 10000));
+        
+        // First pass: Top-left to bottom-right
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+        
+        // Second pass: Bottom-right to top-left
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i < n - 1) {
+                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                }
+                if (j < m - 1) {
+                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+                }
+            }
+        }
+        
+        return dist;
+    }
+};
